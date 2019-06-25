ebd18a514a5ed0d5f3418d9dd23d24e38577e6eb
adrian <eekkaaadrian@gmail.com>
2019-06-25 20:33:51 +0700

    sort by word value

--- helper/helper.go
+++ helper/helper.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"reflect"
 	"regexp"
+	"sort"
 	"strings"
 	"unicode"
 )
@@ -100,3 +101,25 @@ func CalculateRecall(trueSlice interface{}, predictedSlice interface{}) float64 
 	}
 	return truePositive / (truePositive + falseNegative)
 }
+
+func SortByWordValue(wordValue map[string]float64) PairList {
+	pl := make(PairList, len(wordValue))
+	i := 0
+	for k, v := range wordValue {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
+}
+
+type Pair struct {
+	Key   string
+	Value float64
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
